be5e6ad9340f0bf8983c886e5444d7b19d219445
Tatiana Trofimova <nerpassevera@gmail.com>
2024-09-19 15:59:48 -0700

    Adds test

--- main.py
+++ main.py
@@ -5,6 +5,4 @@ def always_returns_true():
     return True
 
 
-def test_always_returns_true():
-    assert always_returns_true()
 
---
+++ test_main.py
@@ -0,0 +1,4 @@
+from main import always_returns_true
+
+def test_always_returns_true():
+    assert always_returns_true()
